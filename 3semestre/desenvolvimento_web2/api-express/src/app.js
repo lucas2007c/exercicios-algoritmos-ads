9468157f22579641c040fedb4a8de3c264bbca3b
lucas2007c <carneiro.g@ifspcaragua.net>
2026-02-24 22:08:57 -0300

    web2: add CRUD endpoints

--- 3semestre/desenvolvimento_web2/api-express/src/app.js
+++ 3semestre/desenvolvimento_web2/api-express/src/app.js
@@ -7,6 +7,26 @@ app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
+app.get('/user', (req, res) => {
+    res.send('GET/ Usuários listados')
+})
+
+app.post('/user', (req, res) => {
+    res.send('POST/ Usuário criado')
+})
+
+app.put('/user', (req, res) => {
+    res.send('PUT/ Usuário atualizado')
+})
+
+app.patch('/user', (req, res) => {
+    res.send('PATCH/ Nome do usuário atualizado')
+})
+
+app.delete('/user', (req, res) => {
+    res.send('DELETE/ Usuário deletado')
+})
+
 app.listen(port, () => {
     console.log(`Servidor rodando em http://localhost:${port}`)
 })
