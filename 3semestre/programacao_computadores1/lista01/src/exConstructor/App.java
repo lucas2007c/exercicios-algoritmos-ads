07252d93e81ffd62187f7d77aeef463d74fcb6fc
lucas2007c <carneiro.g@ifspcaragua.net>
2026-03-02 21:47:31 -0300

    prog1: add constructor example

---
+++ 3semestre/programacao_computadores1/lista01/src/exConstructor/App.java
@@ -0,0 +1,19 @@
+package exConstructor;
+
+public class App {
+    public static void main(String[] args) {
+        Produto p = new Produto("Serra Makita");
+
+        System.out.println("-------------------------------");
+
+        p = new Produto("Parafusadeira", 85f);
+
+        System.out.println("-------------------------------");
+        
+        p = new Produto("Cano pvc", -10f, 20);
+        
+        System.out.println("-------------------------------");
+        Produto p2 = new Produto(p);
+        p2.exibir();
+    }
+}
---
+++ 3semestre/programacao_computadores1/lista01/src/exConstructor/Produto.java
@@ -0,0 +1,45 @@
+package exConstructor;
+
+/*
+Implemente a classe Produto com os atributos nome, preco e quantidade
+Crie:
+Um construtor que receba apenas nome
+Um que receba nome e preço
+Um que receba nome, preço e quantidade
+Use this() para reaproveitar código entre os construtores (chame um construtor dentro do outro). 
+*/
+public class Produto {
+    String nome;
+    float preco;
+    int quantidade;
+
+    public Produto(String nome) {
+        System.out.println("Entrando no construtor 1");
+        this.nome = nome;
+    }
+
+    public Produto(String nome, float preco) {
+        System.out.println("Entrando no construtor 2");
+        this(nome);
+
+        if (preco < 0) {
+            this.preco = 0;
+        } else {
+            this.preco = preco;
+        }
+    }
+
+    public Produto(String nome, float preco, int quantidade) {
+        System.out.println("Entrando no construtor 3");
+        this(nome, preco);
+        this.quantidade = quantidade;
+    }
+
+    public Produto(Produto novoProduto) {
+        this(novoProduto.nome, novoProduto.preco, novoProduto.quantidade);
+    }
+
+    void exibir() {
+        System.out.printf("{\nNome: %s\nPreco: %.2f\nQuantidade: %d\n}", this.nome, this.preco, this.quantidade);
+    }
+}
