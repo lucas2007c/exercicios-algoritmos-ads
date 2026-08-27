8cca484fb0379614604ea80de6a7e9af554a0ee1
lucas2007c <carneiro.g@ifspcaragua.net>
2026-08-27 19:25:22 -0300

    SO: error handling in class Matematica

--- 4semestre/sistemas_operacionais/python/exercicio01/E6matematica.py
+++ 4semestre/sistemas_operacionais/python/exercicio01/E6matematica.py
@@ -2,31 +2,55 @@ from E3fibonacci import fibonacci
 from E5euler import euler
 from fatorial import fatorial
 
+class NegativeValue(Exception):
+    """Valor menor ou igual a zero"""
+
 class Matematica:
     def calcularFibonnaci(self):
-        limite = int(input("Digite até que número deseja gerar a sequência de fibonacci: "))
-        return fibonacci(limite)
+        try:
+            limite = int(input("Digite até que número deseja gerar a sequência de fibonacci: "))
+            if limite <= 0:
+                raise NegativeValue("Insira um número maior que zero.")
+            
+            return fibonacci(limite)
+        except OverflowError:
+            print("Resultado muito longo para ser representado.")
 
     def calcularEuler(self):
-        limite = int(input("Digite quantos números deseja gerar na sequência de euler: "))
-        return euler(limite)
+        try:
+            quantidade = int(input("Digite quantos números deseja gerar na sequência de euler: "))
+            if quantidade <= 0:
+                raise NegativeValue("Insira um número maior que zero.")
+
+            return euler(quantidade)
+        except OverflowError:
+            print("Resultado muito longo para ser representado.")
     
     def calcularFatorial(self):
-        numero = int(input("Digite um número para calcular o fatorial:"))
-        return fatorial(numero)
+        try:
+            numero = int(input("Digite um número para calcular o fatorial:"))
+            if numero <= 0:
+                raise NegativeValue("Insira um número maior que zero.")
+
+            return fatorial(numero)
+        except OverflowError:
+            print("Resultado muito longo para ser representado.")
     
 math = Matematica()
 
-sequenciaFibonacci = math.calcularFibonnaci()
-numerosEuler = math.calcularEuler()
-fatorado = math.calcularFatorial()
+try:
+    sequenciaFibonacci = math.calcularFibonnaci()
+    numerosEuler = math.calcularEuler()
+    fatorado = math.calcularFatorial()
 
-print("Fibonnaci: ")
-for numero in sequenciaFibonacci:
-    print(numero, end=" ")
+    print("Fibonnaci: ")
+    for numero in sequenciaFibonacci:
+        print(numero, end=" ")
 
-print("\n\nEuler:")
-for numero in numerosEuler:
-    print(numero, end=" ")
+    print("\n\nEuler:")
+    for numero in numerosEuler:
+        print(numero, end=" ")
         
-print(f"\n\nFatorial: {fatorado}")
+    print(f"\n\nFatorial: {fatorado}")
+except NegativeValue as error:
+    print(error)
